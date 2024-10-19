4c61b0a162810cc07f5190b42f3a2d3d0ad44fa1
Afrankce <80972479+Afrankce@users.noreply.github.com>
2024-10-19 18:53:23 +0800

    Add files via upload

---
+++ orbit simulation/count.c
@@ -0,0 +1,68 @@
+#include"stdio.h"
+#include <math.h>
+/*  代入初始条件x=3,y=4,z=0.25
+Z(actual)=V0sinθt-1/2gt^2(1式)
+s=1/k1 *In(k1Vx0t+1)（2式）
+代入数据解得12.96=68sinθcosθ+/- sqrt(68^2*sinθ^2-78.4)cosθ
+利用暴力计算得到一个解，即缓慢增大θ
+*/
+void count1()
+{
+    double theta1;
+    for (theta1 = 0; theta1 < 1.5708; theta1 += 0.000001) {
+        double expr1 = 68 * sin(theta1) * cos(theta1);
+        double expr2 = sqrt(68 * 68 * pow(sin(theta1), 2) - 78.4) * cos(theta1);
+        if (fabs(12.96 - (expr1 + expr2)) < 0.01) {
+            printf("theta1 = %lf\n", theta1);
+            break;//这个有解
+        }
+    }
+    
+}
+void count2()
+{
+    double theta2;
+    for (theta2 = 0; theta2 < 1.5708; theta2 += 0.000001) {
+        double expr1 = 68 * sin(theta2) * cos(theta2);
+        double expr2 = sqrt(68 * 68 * pow(sin(theta2), 2) - 78.4) * cos(theta2);
+        if (fabs(12.96 - (expr1 - expr2)) < 0.01) {
+            printf("theta2 = %lf\n", theta2);
+            break;//这个没解
+        }
+    }
+    
+}
+int main(void)
+{ 
+    count1();
+    count2();
+     double s=5.0,targetPoint=0.25,V0=17.0,k=0.038,g=9.8,sina,cosa,t,deltaZ,tempPoint,i,Zactual;
+tempPoint=targetPoint;
+for(i=0;i<10;i++)
+{  cosa=s/sqrt(s*s + tempPoint*tempPoint);
+sina=sqrt(1-cosa*cosa);
+t=(exp(k*s)-1)/(k*V0*cosa);
+Zactual=V0*t*sina-0.5*g*t*t;
+deltaZ=targetPoint-Zactual;
+tempPoint+=deltaZ;
+printf("theta  = %f\n",asin(sina));
+}
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+}
