ec809b37a82284665e33c0cad64fab45e58c03b7
Afrankce <80972479+Afrankce@users.noreply.github.com>
2024-10-20 20:46:46 +0800

    Add files via upload

--- orbit simulation/count.c
+++ orbit simulation/count.c
@@ -4,7 +4,13 @@
 Z(actual)=V0sinθt-1/2gt^2(1式)
 s=1/k1 *In(k1Vx0t+1)（2式）
 代入数据解得12.96=68sinθcosθ+/- sqrt(68^2*sinθ^2-78.4)cosθ
-利用暴力计算得到一个解，即缓慢增大θ
+利用暴力计算得到两个解，即缓慢增大θ
+或者用更加简单的方式：忽略水平阻力，构建抛物线方程
+{V0cosθ*t=s
+V0sinθt-0.5*g*t*t=z
+也可判断有两个解
+}
+
 */
 void count1()
 {
@@ -12,9 +18,10 @@ void count1()
     for (theta1 = 0; theta1 < 1.5708; theta1 += 0.000001) {
         double expr1 = 68 * sin(theta1) * cos(theta1);
         double expr2 = sqrt(68 * 68 * pow(sin(theta1), 2) - 78.4) * cos(theta1);
-        if (fabs(12.96 - (expr1 + expr2)) < 0.01) {
+        if (fabs(12.96 - (expr1 + expr2)) < 0.001) {
             printf("theta1 = %lf\n", theta1);
-            break;//这个有解
+            theta1+=0.01;//保证解不会重复输出
+            //这个有解
         }
     }
     
@@ -25,9 +32,10 @@ void count2()
     for (theta2 = 0; theta2 < 1.5708; theta2 += 0.000001) {
         double expr1 = 68 * sin(theta2) * cos(theta2);
         double expr2 = sqrt(68 * 68 * pow(sin(theta2), 2) - 78.4) * cos(theta2);
-        if (fabs(12.96 - (expr1 - expr2)) < 0.01) {
+        if (fabs(12.96 - (expr1 - expr2)) < 0.001) {
             printf("theta2 = %lf\n", theta2);
-            break;//这个没解
+            theta2+=0.01;
+            //这个没解
         }
     }
     
@@ -36,7 +44,8 @@ int main(void)
 { 
     count1();
     count2();
-     double s=5.0,targetPoint=0.25,V0=17.0,k=0.038,g=9.8,sina,cosa,t,deltaZ,tempPoint,i,Zactual;
+     double s=5.0,targetPoint=0.25,V0=17.0,k=0.038,g=9.8;
+     double sina,cosa,t,deltaZ,tempPoint,i,Zactual;
 tempPoint=targetPoint;
 for(i=0;i<10;i++)
 {  cosa=s/sqrt(s*s + tempPoint*tempPoint);
